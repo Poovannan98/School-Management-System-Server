210c8271c0dce26b6f22d43d9ed036ca67069b88
Poovannan98 <poovannan098@gmail.com>
2023-05-04 22:35:12 +0530

    update server

--- index.js
+++ index.js
@@ -18,6 +18,11 @@ connectDb();
 // ROUTE CONFIGURATION
 app.use("/api/student", studentRoute);
 
+//rest api
+app.get("/", (req, res) => {
+  res.send("<h1>Welcome School Management System App</h1>");
+});
+
 const PORT = process.env.PORT | 5000;
 app.listen(PORT, () => {
   console.log("Backend is running at port ",PORT);
